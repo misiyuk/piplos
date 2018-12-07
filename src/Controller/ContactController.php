92a9393cabde744d9d9718dbe8fa6ec7958f2ea4
ivan <6olinet@gmail.com>
2018-12-07 06:25:08 +0300

    Add contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Forms\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ContactController extends AbstractController
 {
     public function index()
     {
+        $contact = new Contact();
+        $form = $this->createFormBuilder($contact)
+            ->add('name', TextType::class)
+            ->add('email', TextType::class)
+            ->add('subject', TextType::class)
+            ->add('body', TextareaType::class)
+            ->add('save', SubmitType::class, array('label' => 'Отправить'))
+            ->getForm();
         return $this->render('contact/index.html.twig', [
-            'contact' => 'test_contact',
+            'form' => $form->createView(),
         ]);
     }
 }
