a456684dc067d23189dcd99d8c295a5a4e85cf79
ivan <6olinet@gmail.com>
2018-12-08 03:55:23 +0300

    Add api url for axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
